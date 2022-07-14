87a9e1c9272910894a9e4375822e10482c188a80
Rajesh HM <109130293+RajeshMahadevappa@users.noreply.github.com>
2022-07-14 10:05:16 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,12 @@
+#replace  username,pwd, api_key  with yours
+USER_NAME = 'Angel Broker User ID'
+PWD = 'Angle Broker USer Password'
+API_KEY = 'Angle broker API Key'
+
+#Dont change  below varibale 
+FEED_TOKEN = None #Ignore
+TOKEN_MAP = None #Ignore
+SMART_API_OBJ = None #Ignore
+
+
+
